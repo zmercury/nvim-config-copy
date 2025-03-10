0598bbf7da703b5c2f6e5e96b289757c3b562034
zmercury <messiniki@outlook.com>
2025-03-10 15:59:52 +0545

    treesitter lang removed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,21 +5,13 @@ return {
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
 			ensure_installed = {
-				"astro",
 				"cmake",
 				"cpp",
 				"css",
-				"fish",
 				"gitignore",
 				"go",
-				"graphql",
 				"http",
-				"java",
-				"php",
-				"rust",
 				"scss",
-				"sql",
-				"svelte",
 			},
 
 			-- matchup = {
